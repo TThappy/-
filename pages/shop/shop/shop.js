dd3ecd57983c849ad026fa57beeac6f99aed9af8
TThappy <827105427@qq.com>
2019-12-25 15:09:37 +0800

    首页nav完成

--- pages/shop/shop/shop.js
+++ pages/shop/shop/shop.js
@@ -1,56 +1,50 @@
-Page({
-
-  /**
-   * 页面的初始数据
-   */
+Component({
+  options: {
+    addGlobalClass: true,
+  },
   data: {
-    
-    photo:'',
-    name:'',
-    inputShowed: false,
-    inputVal: "",
-    code:'',
-    goods: [{ 'id': 1, 'pic': 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTIxQoibT3WvrjMawopu3yqSfpGvlAEVyvCFSy5CqHTZoghoWjWib9LK86GM10xsJbTWVVp5mKXf0CUA/132', 'name': '校园卡'},
-      { 'id': 2, 'pic': 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTIxQoibT3WvrjMawopu3yqSfpGvlAEVyvCFSy5CqHTZoghoWjWib9LK86GM10xsJbTWVVp5mKXf0CUA/132', 'name': '校园卡'},
-      { 'id': 3, 'pic': 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTIxQoibT3WvrjMawopu3yqSfpGvlAEVyvCFSy5CqHTZoghoWjWib9LK86GM10xsJbTWVVp5mKXf0CUA/132', 'name': '校园卡'}]
+    code: '',
+    TabCur: 0,
+    scrollLeft: 0,
+    navs:[{
+      title:"校园卡",
+      icon: "cuIcon-newsfill"
+    },
+    {
+      title:"钥匙",
+      icon:"cuIcon-lock"
+    },
+    {
+      title:"书籍",
+      icon:"cuIcon-form"
+    },
+    {
+      title:"包",
+      icon:"cuIcon-goods"
+    },
+    {
+      title:"证件",
+      icon:"cuIcon-vipcard"
+    },
+    {
+      title: "手机",
+      icon: "cuIcon-mobile"
+    },
+    {
+      title:"其它",
+      icon:"cuIcon-favor"
+    }
+
+    ]
   },
-  pushgoods: (e) => {
-    wx.navigateTo({
-      url: '../goods/goods',
-    })
-  },
-  toDetailsTap: (e) =>{
-    console.log(e)
-    wx.navigateTo({
-      url: '../goods-details/goods-details?id=' + e.currentTarget.dataset.id,
-    })
-  },
-  search: function (value) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
-      }, 200)
-    })
-  },
-  onLoad() {
-    this.setData({
-      search: this.search.bind(this)
-    })
-  },
-  selectResult: function (e) {
-    console.log('select result', e.detail)
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  attached() {
+    console.log("success")
     var that = this;
-    
     wx.login({
       success: (ret) => {
         console.log(ret)
         that.setData({
-          code:ret.code
+          code: ret.code
         })
         wx.getUserInfo({
           success: (res) => {
@@ -67,62 +61,31 @@ Page({
               }
             })
           },
-          
+
         })
-        
+
       }
 
     })
-   
-      
+    wx.hideLoading()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  methods: {
+    tabSelect(e) {
+      console.log(e)
+      this.setData({
+        TabCur: e.currentTarget.dataset.id,
+        scrollLeft: (e.currentTarget.dataset.id - 1) * 60
+      })
+    }
   },
+  showModal(e) {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
   },
+  hideModal(e) {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
   },
+  showQrcode() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
 })
+
